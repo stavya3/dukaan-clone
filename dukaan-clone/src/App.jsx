ae46c86a4103eb56a71ca7598989e6093d55ad8f
stavya3 <stavyakc03@gmail.com>
2024-09-28 14:12:34 +0530

    Added the revenue card component

--- dukaan-clone/src/App.jsx
+++ dukaan-clone/src/App.jsx
@@ -1,13 +1,16 @@
 
 import './App.css'
+import { RevenueCard } from './Components/RevenueCard'
 
 function App() {
 
 
   return (
-    <>
-      
-    </>
+    <div className='grid grid-cols-3 p-10 gap-5 '>
+      <RevenueCard title={"Next Payout"} amount={"92,312.20"} orderCount={13} />
+      <RevenueCard title={"Amount Pending"} amount={"92,312.20"} orderCount={13} />
+      <RevenueCard title={"Amount Processed"} amount={"23,92,312.19"}/>
+    </div>
   )
 }
 
