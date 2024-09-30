bf0b04568a90c58da3ab9d8e82c0ad505b871c25
stavya3 <stavyakc03@gmail.com>
2024-09-30 21:06:47 +0530

    Added the overview section

--- dukaan-clone/src/App.jsx
+++ dukaan-clone/src/App.jsx
@@ -1,17 +1,18 @@
 
 import './App.css'
 import { BlueCard } from './Components/BlueCard'
+import { Overview } from './Components/Overview'
 import { RevenueCard } from './Components/RevenueCard'
 
 function App() {
 
 
   return (
-    <div className='grid grid-cols-3 p-10 gap-5 '>
-      <BlueCard title={"Next Payout"} amount={"2,312.23"} orderCount={23} />
-      <RevenueCard title={"Amount Pending"} amount={"92,312.20"} orderCount={13} />
-      <RevenueCard title={"Amount Processed"} amount={"23,92,312.19"}/>
+    <div>
+      <Overview />
+      
     </div>
+    
   )
 }
 
