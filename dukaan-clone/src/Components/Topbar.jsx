ac308662ff8d64e4d4b1cc8010d0a32154cb85f5
stavya3 <stavyakc03@gmail.com>
2024-10-01 07:07:00 +0530

    Added some height and width to the topbar component

--- dukaan-clone/src/Components/Topbar.jsx
+++ dukaan-clone/src/Components/Topbar.jsx
@@ -3,7 +3,7 @@
 
 export const Topbar = () => {
     return (
-        <div className="grid grid-cols-3">
+        <div className="grid grid-cols-3 items-center gap-4 h-16">
             <div className="flex items-center gap-4 w-80">
                 
                 <p className="text-xl font-medium">Payouts</p>
