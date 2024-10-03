93c7c7561616e0ede6e4dcd74227b9950b186957
stavya3 <stavyakc03@gmail.com>
2024-10-03 10:04:36 +0530

    Putting all the components together

--- dukaan-clone/src/App.jsx
+++ dukaan-clone/src/App.jsx
@@ -14,13 +14,19 @@ function App() {
 
 
   return (
-    <div>
-      <Topbar />
-      <Overview />
-      <Transactions />
-      <Table />
-      <Foooter />
+    <div className='bg-white-50'>
       <Sidebar />
+      <div className='border-b border-black-150 sm:pl-64 bg-white-500'>
+        <Topbar />
+      </div>
+      <div className='m-5 mb-0 sm:ml-64 grid gap-8'>
+        <Overview />
+        <div className='grid gap-6'>
+          <Transactions />
+          <Table />
+        </div>
+        <Foooter />
+      </div>
     </div>
     
   )
